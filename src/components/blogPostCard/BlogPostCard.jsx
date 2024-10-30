df14edc07d0c529968ea255f914acf735dc3fffc
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-10-30 15:17:06 +0200

    first commit

---
+++ src/components/blogPostCard/BlogPostCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogPostCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BlogPostCard
---
+++ src/components/shareDialogBox/ShareDialogBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShareDialogBox() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShareDialogBox
