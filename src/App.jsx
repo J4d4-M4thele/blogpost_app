28a0d96a59dd1d59df64157cbf64a600c65858f2
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-10-30 15:11:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default App
