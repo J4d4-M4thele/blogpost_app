0b2bda00d6f9f13ab6ad16c4c4e245e4d73ae107
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-11-18 12:29:48 +0200

    update

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer'
 
-function Layout() {
+function Layout({ children }) {
   return (
     <div>
+      <Navbar />
+
+      <div className='content min-h-screen'>
+        {children}
+      </div>
       
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
